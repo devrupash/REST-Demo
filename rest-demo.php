fbd3cac635b2c249d2ca0c96eb12082e52b502ec
Devrupash <rupash.das.202@gmail.com>
2025-04-12 16:38:38 +0600

    Plugin Initialized

---
+++ rest-demo.php
@@ -0,0 +1,16 @@
+<?php 
+/*
+Plugin Name: REST Demo
+Description: REST Demo
+Version: 1.0
+Author: Devrupash
+Author URI: https://devrupash.com
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+text-domain: rest-demo
+Domain Path: /languages
+*/
+class Rest_Demo{
+
+}
+new Rest_Demo();
