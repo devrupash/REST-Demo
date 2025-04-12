e2bfd86b2b6a093ce696fcf1a3015fbf37f8dfc9
Devrupash <rupash.das.202@gmail.com>
2025-04-12 16:54:03 +0600

    Registered hello Route

--- rest-demo.php
+++ rest-demo.php
@@ -11,6 +11,20 @@ text-domain: rest-demo
 Domain Path: /languages
 */
 class Rest_Demo{
-
+	function __construct(){
+		add_action('rest_api_init', array($this,'register_routes'));
+	}
+	function register_routes(){
+		register_rest_route('rest-demo/v1', '/hello', array(
+			'methods' => 'GET',
+			'callback' => array($this,'hello'),
+		));
+	}
+	function hello(){
+		$response = array(
+			"Message" => "Hello World",
+		);
+		return new WP_REST_Response($response, 200);
+	}
 }
 new Rest_Demo();
